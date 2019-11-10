ff21fc6d6d1b8499c7cbd372bc558d1fa5c34c5a
Lissal777 <Lissal777@rambler.ru>
2019-11-10 23:46:39 +0500

    Добавьте файлы проекта.

---
+++ Test_List/GiveMyList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_List
+{
+    class GiveMyList
+    {
+        public GiveMyList GML;
+        public int i;
+
+        public GiveMyList(int i)
+        {
+            this.i = i;
+        }
+
+        public GiveMyList setClass(int i)
+        {
+           return  GML = new GiveMyList(i); 
+        }
+    }
+}
---
+++ Test_List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GiveMyList cur;
+            GiveMyList GM = new GiveMyList(0);
+            cur = GM.setClass(1);
+            for (int i = 2; i < 10; i++)
+            {
+                cur = cur.setClass(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(GM.i);
+                GM = GM.GML;
+            }
+            Console.ReadKey();
+        }
+    }
+}
